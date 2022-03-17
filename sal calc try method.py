4cd1092e682484c3f7f5ddd88aa1a516a7ae5a8a
BruhMomentos <k.nguyen2003@hotmail.com>
2022-03-16 17:11:28 -0700

    new sal calc with try

---
+++ sal calc try method.py
@@ -0,0 +1,23 @@
+
+try:
+    hour = int(input("Enter the amount of hours you work "))
+    rate = int(input("Enter your rate of pay "))
+except:
+    print(("Please Enter a Numeric Input"))
+    quit()
+
+pay = hour * rate
+spay = str(pay)
+
+if hour > 40:
+    otr = (hour - 40) * (rate * 1.5)
+    otp = otr + (40 * rate)
+    print("You worked over 40 hours, Your Overtime pay is " + str(otp))
+else:
+    print("You worked a regular pay of $" + spay)
+
+input("Press key to close")
+
+
+
+
